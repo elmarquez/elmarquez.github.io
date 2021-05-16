14915f61d9ded9d4b133d1cbe84458b9529590a3
Davis Marques <dmarq.ezz@gmail.com>
2021-05-16 21:42:01 +1000

    Update website

---
+++ work/four/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import GlobalStyle, { BlogLayout, ColumnLayout } from "../../../styles";
+
+const ProjectPage = () => (
+  <React.Fragment>
+    <GlobalStyle />
+    <BlogLayout>
+      <ColumnLayout>
+        <p>
+            FOUR demos
+        </p>
+      </ColumnLayout>
+    </BlogLayout>
+  </React.Fragment>
+);
+
+export default ProjectPage;
+
